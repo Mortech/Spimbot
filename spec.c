3bba8e476405c781785483abd8bc8cf7ca0e17b4
Fantin <bapgamer23@gmail.com>
2012-12-07 20:26:42 -0600

    working optimized sort

--- spec.c
+++ spec.c
@@ -1,6 +1,6 @@
 /*
 
-  Spinbot speces people should know.
+  Spimbot specs people should know.
   if you need to make new data structer put it here 
   so people will know how to use it
 
@@ -9,15 +9,15 @@
 
   
   $s5 - $s7 and $t7 - $t10 are not to be used by
-  or stored away in funtions. They are saved for use as defind here:
+  or stored away in funtions. They are saved for use as defined here:
 
   $s5: the head of the token list
   $s6: the tail of the token list 
 
-	$t8 and $t10: used in sort_list, should be restored when interrupting
+	$t8 and $t10: used in sort_list, should be restored if interrupted
 	$t9: constant value - 1
 
-  more to come use on code you need to run alot.
+  more to come use on code you need to run a lot.
 
 
 	MAKE SURE that new nodes added to the list have their next point to 0!
